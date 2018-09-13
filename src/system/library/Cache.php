bd01e8f25fddc78c060cde7839acc0e296992ed4
kaixin <kaixinco@126.com>
2018-09-14 00:38:01 +0800

    'add'

---
+++ src/system/library/Cache.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace system\library;
+
+class Cache
+{
+    private $_cache;
+
+    public function __construct($driver, $expire = 3600)
+    {
+
+    }
+
+    public function get($key)
+    {
+    }
+
+    public function set($key, $value)
+    {
+    }
+
+    public function delete($key)
+    {
+    }
+}
---
+++ src/system/library/Log.php
@@ -0,0 +1,19 @@
+<?php
+namespace system\library;
+
+class Log
+{
+    private $_handle;
+
+    public function __construct($filename)
+    {
+    }
+
+    public function write($message)
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+}
